f144fa2c27a4ff384dd4259c197803d0f858b539
kimhaerim <34bestsun@gamil.com>
2022-03-02 12:25:23 +0900

    BFS&DFS

---
+++ BAEKJOON_1260/cmake-build-debug/CMakeFiles/BAEKJOON_1260.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BAEKJOON_1260"
+  "BAEKJOON_1260.pdb"
+  "CMakeFiles/BAEKJOON_1260.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BAEKJOON_1260.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BAEKJOON_9184/cmake-build-debug/CMakeFiles/BAEKJOON_9184.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BAEKJOON_9184"
+  "BAEKJOON_9184.pdb"
+  "CMakeFiles/BAEKJOON_9184.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BAEKJOON_9184.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
