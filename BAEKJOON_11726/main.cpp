86930df84cb2a03a65e1daec2d2e5876fb418807
kimhaerim <34bestsun@gmail.com>
2022-03-05 21:17:29 +0900

    Stack구현

--- BAEKJOON_11726/main.cpp
+++ BAEKJOON_11726/main.cpp
@@ -3,19 +3,17 @@
 
 using namespace std;
 
-
 int main() {
     int n;
-
     cin >> n;
-    int result[n];
-    memset(result, 0, n);
 
-    result[0] = 1;
-    result[1] = 2;
-    for(int i = 2; i <n; i++){
-        result[i] = (result[i-2] + result[i-1]) % 10007;
+    int dp[n + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007;;
     }
-    cout << result[n-1];
+
+    cout << dp[n];
     return 0;
 }
