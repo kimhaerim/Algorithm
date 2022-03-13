3b52ea5e16e3a92501dd2b0f3d4620695b3d67e0
Haerim <71517226+kimhaerim@users.noreply.github.com>
2022-03-13 17:04:26 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    priority_queue<int, vector<int>, greater<int>> arr;
+    int num;
+
+    for(int i = 0; i < n; i++){
+        cin >> num;
+        if(num == 0){
+            if(arr.empty()){
+                cout << "0\n";
+            }
+            else{
+                cout << arr.top() << "\n";
+                arr.pop();
+            }
+        }
+
+        else {
+            arr.push(num);
+        }
+    }
+    return 0;
+}
