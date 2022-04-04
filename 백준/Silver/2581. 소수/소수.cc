475afda7a4051c5c65fe2644832198c0e53944d9
Haerim <71517226+kimhaerim@users.noreply.github.com>
2022-04-04 14:08:24 +0900

    Update 소수.cc

--- 백준/Silver/2581. 소수/소수.cc
+++ 백준/Silver/2581. 소수/소수.cc
@@ -2,30 +2,39 @@
 #include <cmath>
 using namespace std;
 
-int main() {
-    int min,max,sum = 0;
-    int min_value;
-    bool prime[10000];
-    fill_n(prime, 10000, 1);
-    prime[0] = false;
-    prime[1] = false;
-    cin >> min;
-    cin >> max;
-    for (int i = 2; i <= sqrt(max); i++)
-        if(prime[i] == true)
-            for(int j = i*2; j <= max; j += i)
-                prime[j] = false;
 
-    for (int i = min; i <= max; i++)
-        if(prime[i] == true)
-        {
-            if(sum == 0)
-                min_value = i;
+int main() {
+    bool check[10000] = {true, };
+
+    check[0] = false;
+    check[1] = false;
+
+    int sum = 0;
+    int min = -1;
+    int m, n;
+    cin >> m >> n;
+
+    for (int i = 2; i <= sqrt(n); i++) {
+        if(check[i]){
+            for (int j = i + i; j <= n; j+= i) {
+                check[j] = false;
+            }
+        }
+    }
+
+    for (int i = m; i <= n; i++) {
+        if(check[i]) {
+            if(sum == 0) {
+                min = i;
+            }
             sum += i;
         }
-    if(sum == 0)
-        cout << "-1";
+    }
+
+    if(sum != 0) {
+        cout << sum << "\n" << min;
+    }
     else
-        cout << sum << '\n' << min_value;
+        cout << -1;
     return 0;
 }
